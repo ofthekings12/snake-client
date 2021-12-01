c40589fb0c60ffe2be81329cf3eb5084f001adc7
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-01 21:22:08 +0000

    snek

---
+++ play.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+conn.on('connect', () => {
+  console.log("successfully connected to game server")
+});
+
+conn.on('connection', () => {
+  conn.write('Name: OTK');
+});
+
+conn.on('data',(data) => {
+  console.log(data);
+});
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
