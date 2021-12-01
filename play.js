3d2be252c5fc90056774f911aa29ae22ba591a02
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-01 22:22:13 +0000

    exported connect function to client

--- play.js
+++ play.js
@@ -1,18 +1,8 @@
-const net = require("net");
+const connectionFunctions = require('./client')
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
-  });
 
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-
-    return conn;
-  };
-
+const connect = connectionFunctions.connect;
 const conn = connect();
 
 conn.on('connect', () => {
