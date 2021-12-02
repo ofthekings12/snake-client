a0831eb5403e0cc353105aab70ca85f10c22d6f6
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-02 00:00:05 +0000

    ctrl + c exits game

--- play.js
+++ play.js
@@ -1,7 +1,26 @@
 const connectionFunctions = require('./client')
 
-// establishes a connection with the game server
 
+const handleUserInput = function (data) { // data will be whatever the user pressed on their keyboard in a utf8 string ---> https://www.utf8-chartable.de/
+  if (data === "\u0003") {
+    process.exit(); // leave the game if the user presses ctrl + c
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput(); // sets up event listener for user keypresses 🗿
+
+// establishes a connection with the game server
 const connect = connectionFunctions.connect;
 const conn = connect();
 
@@ -9,10 +28,13 @@ conn.on('connect', () => {
   console.log('New client connected!')
   conn.write("Name: OTK");
   conn.write("Hey");
-  // conn.write("Move: left")
+  conn.write("move: up")
+  
   // conn.write("Move: left")
   // conn.write("Move: left")
 });
+
+
 
 
 
