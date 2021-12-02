f11ac40e41c4f9ae9964080564ed19a5229222c2
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-02 01:23:46 +0000

    snek sings rupaul lyrics

--- input.js
+++ input.js
@@ -15,6 +15,37 @@ const handleUserInput = function (data, conn) { // data will be whatever the use
   if (data === "d") {
     conn.write("Move: right")
   }
+  if (data === "1") {
+    conn.write("Say: GUESS WHOS BACK IN")
+  }
+  if (data === "2") {
+    conn.write("Say: THE HOUSE HEELS CLICK")
+  }
+  if (data === "3") {
+    conn.write("Say: CLACKIN ABOUT")
+  }
+  if (data === "4") {
+    conn.write("Say: FINE FRESH FEMININE")
+  }
+  if (data === "5") {
+    conn.write("Say: STYLE TO ELEVEN")
+  }
+  if (data === "6") {
+    conn.write("Say: I'M DIVINE SO HEAVENLY")
+  }
+  if (data === "7") {
+    conn.write("Say: GENTLEMEN SWEATIN ITS DIMES")
+  }
+  if (data === "8") {
+    conn.write("Say: ACROSS THE BOARD")
+  }
+  if (data === "9") {
+    conn.write("Say: NO DOUBT")
+  }
+  if (data === "0") {
+    conn.write("Say: BODY LIKE: WWOOOWWWW!")
+  }
+
 };
 
 
